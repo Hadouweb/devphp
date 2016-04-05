0bbae6c7c8174c5cf27da4c38c5567d2902e725b
Hadouweb <kengo14@hotmail.fr>
2016-04-05 16:44:55 +0200

    Correction

--- d01/ex10/do_op.php
+++ d01/ex10/do_op.php
@@ -2,9 +2,27 @@
 <?php
 if ($argc == 4)
 {
-	$op = $argv[1].$argv[2].$argv[3];
-	$result = eval('return '.$op.';');
-	echo "$result\n";
+	if ($argv[2][0] == '+')
+		echo (int)$argv[1] + (int)$argv[3];
+	else if ($argv[2][0] == '-')
+		echo (int)$argv[1] - (int)$argv[3];
+	else if ($argv[2][0] == '*')
+		echo (int)$argv[1] * (int)$argv[3];
+	else if ($argv[2][0] == '/')
+	{
+		if ($argv[3] == '0')
+			echo "Error : division by zero";
+		else
+			echo (int)$argv[1] / (int)$argv[3];
+	}
+	else if ($argv[2][0] == '%')
+	{
+		if ($argv[3] == '0')
+			echo "Error : division by zero";
+		else
+			echo (int)$argv[1] % (int)$argv[3];
+	}
+	echo "\n";
 }
 else
 	echo "Incorrect Parameters\n";
