6009d224f3221d61996ce5e64bbefc4d5d22077f
Hadouweb <kengo14@hotmail.fr>
2016-04-06 15:40:10 +0200

    ex01

---
+++ d02/ex01/one_more_time.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+date_default_timezone_set('Europe/Paris');
+setlocale(LC_TIME, "fr_FR");
+if ($argc > 1)
+{
+	$a = explode(' ',$argv[1]);
+	$d = strptime($argv[1], '%A %d %B %Y %T');
+	$date = mktime($d[tm_hour], $d[tm_min], $d[tm_sec], $d[tm_mon]+1,
+		$d[tm_mday], $d[tm_year]+1900);
+	if ($d == false)
+		echo "Wrong Format\n";
+	else
+		echo $date."\n";
+}
+?>
