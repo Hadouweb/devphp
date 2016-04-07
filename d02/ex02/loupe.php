0b2f8c3ff4b61802bd3fb3495796fb0bc0074c8f
hadouweb <kengo14@hotmail.fr>
2016-04-07 03:33:21 +0200

    ex02 ok

--- d02/ex02/loupe.php
+++ d02/ex02/loupe.php
@@ -2,9 +2,14 @@
 <?php
 function replace_toupper($matches) 
 {
-	var_dump($matches);
-	//$final = preg_replace("/$matches[1]/", strtoupper($matches[1]), $matches[0]);
-	return $final;
+	preg_match_all("!<[^>]+>!is", $matches[2], $balises);
+	$final = preg_replace("!$matches[2]!is", strtoupper($matches[2]), $matches);
+	foreach($balises[0] as $balise)
+		$final = preg_replace("!$balise!is", strtolower($balise), $final);
+	preg_match_all("!title=\"(.*?)\"!is", $final[0], $titles);
+	foreach($titles[1] as $title)
+		$final = preg_replace("!$title!is", strtoupper($title), $final);
+	return $final[1].$final[2].$final[3];
 }
 
 if ($argv > 1)
@@ -14,9 +19,7 @@ if ($argv > 1)
 		echo "Impossible d'oubrir le fichier\n";
 	else
 	{
-		//'/title=\"(.*)\"/Uis'
-		$pattern = array('!<a[^>]*>(.*)</a>!Uis');
-		$page = preg_replace_callback($pattern, 
+		$page = preg_replace_callback('!(<a.*?>)(.*?)(</a>)!is', 
 			"replace_toupper",
 			$page
 		);
