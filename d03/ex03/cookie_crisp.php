0ce76e3618ed16d640c48df23e09744dc7f7a8d1
Hadouweb <kengo14@hotmail.fr>
2016-04-07 13:52:32 +0200

    d03

--- d03/ex03/cookie_crisp.php
+++ d03/ex03/cookie_crisp.php
@@ -1,6 +1,6 @@
 <?php
 if ($_GET['action'] == "set")
-   setcookie($_GET['name'], $_GET['value'], time() + 60 * 60 * 24 * 30);
+   setcookie($_GET['name'], $_GET['value'], 0);
 else if ($_GET['action'] == "get")
 {
 	$tmp = $_COOKIE[$_GET['name']];
@@ -8,5 +8,5 @@ else if ($_GET['action'] == "get")
 	   echo $tmp."\n";
 }
 else if ($_GET['action'] == "del")
-	 setcookie($_GET['name'], "", time() - 5);
+	 setcookie($_GET['name'], "", time() - 3600);
 ?>
