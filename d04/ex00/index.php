e4b47c2a45c62dae1423eb1471f5e4492a4069b1
Hadouweb <kengo14@hotmail.fr>
2016-04-08 09:19:16 +0200

    ex00

--- d04/ex00/index.php
+++ d04/ex00/index.php
@@ -1,10 +1,13 @@
 <?php
 	session_start();
-	if ($_GET['login'] && $_GET['passwd'] && ($_GET['submit'] == "OK"))
+	if (isset($_GET['login']) && isset($_GET['passwd']) 
+		&& ($_GET['submit'] == "OK"))
 	{
-			$_SESSION['login'] = $_GET['login'];
-			$_SESSION['passwd'] = $_GET['passwd'];
+		$_SESSION['login'] = $_GET['login'];
+		$_SESSION['passwd'] = $_GET['passwd'];
 	}
+	var_dump($_SESSION);
+	var_dump($_GET);
 ?>
 
 <html>
@@ -14,16 +17,10 @@
 <body>
 	<form method="get" action="index.php">
 		<p>Identifiant <input type="text" name="login" value="<?php 
-		if ($_SESSION['login'] === $_GET['login'])
 			echo ($_SESSION['login']);
-		else
-			echo "";
 		?>" /></p>
 		<p>Mot de passe <input type="password" name="passwd" value="<?php 
-		if ($_SESSION['passwd'] === $_GET['passwd'])
 			echo ($_SESSION['passwd']);
-		else
-			echo "";
 		?>" /></p>
 		<input type="submit" name="submit" value="OK"/>
 	</form>
