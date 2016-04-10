0ec8f15d29ab86ef73c5f3ee5266e6a40dcd0883
Hadouweb <kengo14@hotmail.fr>
2016-04-10 11:41:19 +0200

    install

--- rush00/install.php
+++ rush00/install.php
@@ -8,16 +8,16 @@ $dbname = "db_rush";
 @$conn = mysqli_connect($servername, $username, $password);
 // Check connection
 if (mysqli_connect_errno())
-  die("Failed to connect to MySQL: " . mysqli_connect_error(). "<br />");
+	die("Failed to connect to MySQL: " . mysqli_connect_error(). "<br />");
 else
-  echo "MySql connection successfully<br />";
+	echo "MySql connection successfully<br />";
 
 // Delete database
 $sql = "DROP DATABASE $dbname";
 if(mysqli_query($conn, $sql) === TRUE)
-  echo "Database delete successfully<br />";
+	echo "Database delete successfully<br />";
 else
-  echo "Error delete database: " . mysqli_error($conn) . "<br />";
+	echo "Error delete database: " . mysqli_error($conn) . "<br />";
 
 
 // Create database
@@ -29,15 +29,18 @@ else
 
 mysqli_close($conn);
 $db = mysqli_connect($servername, $username, $password, $dbname);
+if (mysqli_connect_errno())
+	die("Failed to connect to MySQL: " . mysqli_connect_error(). "<br />");
+else
+	echo "MySql connection successfully<br />";
 mysqli_set_charset($db,"utf8");
 
 // Create user table
 $sql = "CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_type` int NOT NULL,
+  `user_role` int NOT NULL,
   `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `is_logged` BOOLEAN NOT NULL,
+  `password` varchar(2000) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
@@ -49,8 +52,8 @@ else
 // Create order table
 $sql = "CREATE TABLE `order` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `order_detail_id` int NOT NULL,
-  `total` FLOAT(10) NOT NULL,
+  `product_id` int NOT NULL,
+  `quantity` int NOT NULL,
   `user_id` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
@@ -60,7 +63,7 @@ else
     echo "Error creating order table: " . mysqli_error($db) . "<br />";
 
 // Create order detail table
-$sql = "CREATE TABLE `order_detail` (
+/*$sql = "CREATE TABLE `order_detail` (
   `id` int NOT NULL AUTO_INCREMENT,
   `product_id` text NOT NULL,
   `quantity` text NOT NULL,
@@ -70,7 +73,7 @@ $sql = "CREATE TABLE `order_detail` (
 if (mysqli_query($db, $sql) === TRUE)
     echo "Order table created successfully<br />";
 else
-    echo "Error creating order detail table: " . mysqli_error($db) . "<br />";
+    echo "Error creating order detail table: " . mysqli_error($db) . "<br />";*/
 
 // Category table
 $sql = "CREATE TABLE `category` (
@@ -94,7 +97,7 @@ $sql = "CREATE TABLE `product` (
   `price` FLOAT(10) NOT NULL,
   `picture` varchar(1000) NOT NULL,
   `category_id` int DEFAULT 0,
-  `date_added` DATE NOT NULL,
+  `date_added` DATETIME NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 if (mysqli_query($db, $sql) === TRUE)
@@ -105,7 +108,7 @@ else
 // _______________________________ INSERT ___________________________________
 
 // Insert Super user Nicolas
-$sql = "INSERT INTO `user` (`user_type`, `username`, `password`, `is_logged`) VALUES 
+$sql = "INSERT INTO `user` (`user_role`, `username`, `password`, `is_logged`) VALUES 
 ('1', 'Nicolas', 'mdp', 0),
 ('1', 'Samuel', 'mdp', 0)";
 if (mysqli_query($db, $sql) === TRUE)
@@ -127,12 +130,11 @@ else
 // Insert Product
 $sql = "INSERT INTO `product` (`product_name`, `product_desc`, `stock`, `price`, `picture`, `category_id`, `date_added`) VALUES
 ('UNCHARTED THE NATHAN DRAKE COLLECTION', 'Les trois premiers épisodes de la saga Uncharted sont là dans une version remasterisée en 1080p / 60 fps.', '10', '39.99', 'http://www.gamer-network.fr/wp-content/uploads/2015/08/Uncharted-The-Nathan-Drake-Collection-trois-modes-de-jeu-Image-2.png', '1', '2016-04-09'),
-('STAR WARS BATTLEFRONT', 'La force est avec DICE', '10', '49.99', 'https://media.starwars.ea.com/content/starwars-ea-com/fr_FR/starwars/battlefront/_jcr_content/ogimage.img.jpeg', '1', '2016-04-09'),
-('WORLD OF WARCRAFT : WARLORDS OF DRAENOR', 'On l\'avait prévenu, Thrall : Garrosh, fallait pas l\'inviter !', '10', '9.99', 'http://media.blizzard.com/wow/warlords-of-draenor-6y1fz/media/wallpapers/warlords-of-draenor-1680x1050.jpg', '4', '2016-04-09')";
+('STAR WARS BATTLEFRONT', 'La force est avec DICE', '10', '49.99', 'https://media.starwars.ea.com/content/starwars-ea-com/fr_FR/starwars/battlefront/_jcr_content/ogimage.img.jpeg', '1', '2016-04-09')";
 if (mysqli_query($db, $sql) === TRUE)
     echo "Products inserted successfully<br />";
 else
     echo "Error inserting products: " . mysqli_error($db) . "<br />";
 
-
+mysqli_close($db);
 ?>
